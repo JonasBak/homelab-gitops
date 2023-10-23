2e71b7f04b186916f957cd2bf04db7814dbabb30
Jonas Bakken <jonasbakken@protonmail.com>
2023-10-23 20:26:19 +0200

    chore: put systemd file generation in systemd_services.go

---
+++ systemd_services.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+func GenerateContainerFile(manifest Manifest, service string, hash string, templateKV map[string]string) string {
+	containerFields := buildFields(manifest.Container, templateKV)
+	unitFields := buildFields(manifest.Unit, templateKV)
+	serviceFields := buildFields(manifest.Service, templateKV)
+
+	file := fmt.Sprintf(`
+[Install]
+WantedBy=default.target
+
+[Container]
+%s
+Label=gitops-service=%s
+Label=gitops-hash=%s
+
+[Unit]
+%s
+
+[Service]
+%s
+`, containerFields, service, hash, unitFields, serviceFields)
+
+	return file
+}
+
+func GenerateNetworkFile(kvs map[string][]string, networkName string) string {
+	networkFields := buildFields(kvs, make(map[string]string))
+
+	sha := sha256.New()
+	_, _ = sha.Write([]byte(networkFields))
+	hash := hex.EncodeToString(sha.Sum(nil))
+
+	file := fmt.Sprintf(`
+[Network]
+%s
+Label=gitops-network=%s
+Label=gitops-hash=%s
+`, networkFields, networkName, hash)
+
+	return file
+}
+
+func GenerateVolumeFile(kvs map[string][]string, volumeName string) string {
+	volumeFields := buildFields(kvs, make(map[string]string))
+
+	sha := sha256.New()
+	_, _ = sha.Write([]byte(volumeFields))
+	hash := hex.EncodeToString(sha.Sum(nil))
+
+	file := fmt.Sprintf(`
+[Volume]
+%s
+Label=gitops-volume=%s
+Label=gitops-hash=%s
+`, volumeFields, volumeName, hash)
+
+	return file
+}
--- utils.go
+++ utils.go
@@ -161,30 +161,6 @@ func buildFields(kvs map[string][]string, templateKV map[string]string) string {
 	return fields
 }
 
-func GenerateContainerFile(manifest Manifest, service string, hash string, templateKV map[string]string) string {
-	containerFields := buildFields(manifest.Container, templateKV)
-	unitFields := buildFields(manifest.Unit, templateKV)
-	serviceFields := buildFields(manifest.Service, templateKV)
-
-	file := fmt.Sprintf(`
-[Install]
-WantedBy=default.target
-
-[Container]
-%s
-Label=gitops-service=%s
-Label=gitops-hash=%s
-
-[Unit]
-%s
-
-[Service]
-%s
-`, containerFields, service, hash, unitFields, serviceFields)
-
-	return file
-}
-
 func readManifest(serviceDir string) (Manifest, error) {
 	config := Manifest{
 		make(map[string][]string),
@@ -228,38 +204,4 @@ func readManifest(serviceDir string) (Manifest, error) {
 	}
 
 	return config, nil
-}
-
-func GenerateNetworkFile(kvs map[string][]string, networkName string) string {
-	networkFields := buildFields(kvs, make(map[string]string))
-
-	sha := sha256.New()
-	_, _ = sha.Write([]byte(networkFields))
-	hash := hex.EncodeToString(sha.Sum(nil))
-
-	file := fmt.Sprintf(`
-[Network]
-%s
-Label=gitops-network=%s
-Label=gitops-hash=%s
-`, networkFields, networkName, hash)
-
-	return file
-}
-
-func GenerateVolumeFile(kvs map[string][]string, volumeName string) string {
-	volumeFields := buildFields(kvs, make(map[string]string))
-
-	sha := sha256.New()
-	_, _ = sha.Write([]byte(volumeFields))
-	hash := hex.EncodeToString(sha.Sum(nil))
-
-	file := fmt.Sprintf(`
-[Volume]
-%s
-Label=gitops-volume=%s
-Label=gitops-hash=%s
-`, volumeFields, volumeName, hash)
-
-	return file
 }
